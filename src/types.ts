a0bfc1fac70b6c086e04bbb0d2e8e8a65fa9556e
Firekid846 <firekideuacc@gmail.com>
2026-07-01 21:48:21 +0100

    feat: add types for staleWhileRevalidate, shouldCache, store, abort, refresh, stats

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,27 @@
 export type AnyAsyncFn<T> = (...args: any[]) => Promise<T>
 
-export interface OnceOptions<TArgs extends any[]> {
+export interface OnceCacheEntry<T> {
+  promise: Promise<T> | null
+  expiresAt: number | null
+  value?: T
+  hasValue: boolean
+  revalidating: boolean
+}
+
+export interface OnceStats {
+  hits: number
+  misses: number
+  dedup: number
+}
+
+export interface OnceOptions<TArgs extends any[], TResult = any> {
   key?: string | ((...args: TArgs) => string)
   ttl?: number
   maxKeys?: number
   onDeduplicated?: (key: string) => void
+  staleWhileRevalidate?: boolean
+  shouldCache?: (value: TResult) => boolean
+  store?: Map<string, OnceCacheEntry<TResult>>
 }
 
 export interface OnceInstance<TArgs extends any[], TResult> {
@@ -12,9 +29,13 @@ export interface OnceInstance<TArgs extends any[], TResult> {
   clear: (key?: string) => void
   size: () => number
   has: (key: string) => boolean
+  abort: (key?: string) => void
+  refresh: (...args: TArgs) => Promise<TResult>
+  stats: () => OnceStats
 }
 
 export interface CreateOnceOptions {
   maxKeys?: number
   ttl?: number
+  staleWhileRevalidate?: boolean
 }
